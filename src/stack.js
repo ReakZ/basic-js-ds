6651ce59fe0e28dc21a189aa1593058162fb19a4
ReakZ <spec.kov@list.ru>
2022-05-01 08:50:07 +0300

    feat: implement stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
   push(/* element */) {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(arguments[0]);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.stack.length) return undefined;
+    return this.stack.splice(this.stack.length - 1, 1)[0];
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
